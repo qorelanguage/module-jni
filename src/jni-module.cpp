95f5872bc1758168188f5bac4d43e46ab1c54a75
Ondra Tethal <ondrej.tethal@qoretechnologies.com>
2016-08-30 11:31:52 +0200

    Initial commit - empty module

---
+++ src/jni-module.cpp
@@ -0,0 +1,66 @@
+/*
+  jni-module.cpp
+
+  JNI integration to Qore
+
+  Qore Programming Language
+
+  Copyright (C) 2016 Qore Technologies, sro
+
+  Permission is hereby granted, free of charge, to any person obtaining a
+  copy of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  Note that the Qore library is released under a choice of three open-source
+  licenses: MIT (as above), LGPL 2+, or GPL 2+; see README-LICENSE for more
+  information.
+*/
+#include <qore/Qore.h>
+
+static QoreNamespace JniNamespace("JNI");
+
+static QoreStringNode *jni_module_init();
+static void jni_module_ns_init(QoreNamespace *rns, QoreNamespace *qns);
+static void jni_module_delete();
+
+DLLEXPORT char qore_module_name[] = "jni";
+DLLEXPORT char qore_module_version[] = PACKAGE_VERSION;
+DLLEXPORT char qore_module_description[] = "JNI module";
+DLLEXPORT char qore_module_author[] = "Qore Technologies, sro";
+DLLEXPORT char qore_module_url[] = "http://qore.org";
+DLLEXPORT int qore_module_api_major = QORE_MODULE_API_MAJOR;
+DLLEXPORT int qore_module_api_minor = QORE_MODULE_API_MINOR;
+DLLEXPORT qore_module_init_t qore_module_init = jni_module_init;
+DLLEXPORT qore_module_ns_init_t qore_module_ns_init = jni_module_ns_init;
+DLLEXPORT qore_module_delete_t qore_module_delete = jni_module_delete;
+#ifdef _QORE_HAS_QL_MIT
+DLLEXPORT qore_license_t qore_module_license = QL_MIT;
+#else
+DLLEXPORT qore_license_t qore_module_license = QL_LGPL;
+#endif
+DLLEXPORT char qore_module_license_str[] = "MIT";
+
+static QoreStringNode *jni_module_init() {
+   return 0;
+}
+
+static void jni_module_ns_init(QoreNamespace *rns, QoreNamespace *qns) {
+   qns->addInitialNamespace(JniNamespace.copy());
+}
+
+static void jni_module_delete() {
+}
