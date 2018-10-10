1d8f448e2894509e020e503b23889ece1d07b366
David Nichols <david@qore.org>
2018-10-10 07:02:32 +0200

    refs qorelanguage/qore#3026 changed QoreObject.getClassName() -> className() to align with the Qore API

--- src/java/org/qore/jni/QoreObject.java
+++ src/java/org/qore/jni/QoreObject.java
@@ -1,18 +1,22 @@
 package org.qore.jni;
 
+//! wrapper class for a Qore object; this class holds a weak reference to the Qore object
 public class QoreObject {
+    //! a pointer to the Qore object
     private long obj;
 
+    //! creates the wrapper object with a pointer to an object; this JAva object holds a weak reference to the Qore object passed here
     public QoreObject(long obj) {
         this.obj = obj;
     }
 
+    //! returns the pointer to the object
     public long get() {
         return obj;
     }
 
     //! returns the class name for the object
-    public String getClassName() {
+    public String className() {
         return getClassName0(obj);
     }
 
@@ -33,26 +37,34 @@ public class QoreObject {
 
     //! releases the Qore object without destroying it
     public void release() {
-        long x = obj;
-        obj = 0;
-        release0(x);
+        long x = releasePointer();
+        if (x != 0) {
+            release0(x);
+        }
     }
 
     //! runs the destructor
     public void destroy() {
-        if (obj != 0) {
-            long x = obj;
-            obj = 0;
+        long x = releasePointer();
+        if (x != 0) {
             destroy0(x);
         }
     }
 
+    //! releases the weak reference
     @Override
     protected void finalize() throws Throwable {
         if (obj != 0) {
             finalize0(obj);
             obj = 0;
         }
+    }
+
+    //! clears the internal pointer and returns the pointer value as a long
+    private synchronized long releasePointer() {
+        long x = obj;
+        obj = 0;
+        return x;
     }
 
     private native String getClassName0(long obj);
