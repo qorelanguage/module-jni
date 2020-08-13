3c94b42b79e781f011b7741ff0e88b47bc800af6
David Nichols <david.nichols@qoretechnologies.com>
2020-08-13 12:21:00 +0200

    refs qorelanguage/qore#3876 updated docs for pipeline multi output, added QoreException constructor (desc optional), updated impl, added to docs

--- src/java/org/qore/jni/QoreException.java
+++ src/java/org/qore/jni/QoreException.java
@@ -2,9 +2,13 @@ package org.qore.jni;
 
 import java.util.Arrays;
 
+//! The QoreException class is meant to represent a %Qore-language exception
 public class QoreException extends RuntimeException {
+    //! The %Qore exception error code
     private String err;
+    //! The %Qore exception description; if any
     private String desc;
+    //! The %Qore exception argument; if any
     private Object arg;
 
     private static final long serialVersionUID = 1L;
@@ -22,12 +26,17 @@ public class QoreException extends RuntimeException {
         this.desc = desc;
     }
 
+    //! creates the object with the given arguments
+    public QoreException(String err) {
+        this.err = err;
+    }
+
     //! returns the error string
     String getErr() {
         return err;
     }
 
-    //! returns the description string
+    //! returns the description string, if any; may be null
     String getDesc() {
         return desc;
     }
